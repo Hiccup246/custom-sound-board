07b58e86dc7024a09afd749d13a129f0c08eaa6b
James Watt <james.watt529@outlook.com>
2022-12-07 20:13:13 +0000

    Fix padding between footer items

--- components/SoundBoardFooter.tsx
+++ components/SoundBoardFooter.tsx
@@ -5,19 +5,19 @@ import ThemeToggle from "@/islands/ThemeToggle.tsx";
 export function SoundBoardFooter() {
     return (
         <footer class="mx-auto dark:bg-dark-black bg-light-pink flex flex-wrap mb-2 justify-center">
-            <a href="https://fresh.deno.dev" class="my-auto w-52 flex place-content-center hover:underline">
-                <FreshIcon/><p class="my-auto mr-5">Made with <b>Fresh</b></p>
+            <a href="https://fresh.deno.dev" class="my-auto w-52 flex place-content-center hover:underline mb-1.5 sm:mb-auto">
+                <FreshIcon /><p class="my-auto mr-5">Made with <b>Fresh</b></p>
             </a>
 
             <a
-            class="hover:underline flex gap-2 items-center dark:text-white text-black my-auto w-52 place-content-center"
+            class="hover:underline flex gap-2 items-center dark:text-white text-black my-auto mb-4 sm:mb-auto w-52 place-content-center"
             href="https://github.com/Hiccup246/sound-board"
             >
                 <GitHubIcon class="h-5 w-5 dark:text-gray-500 dark:text-white text-black" />
                 Source
             </a>
 
-            <div class="w-52 my-auto flex place-content-center">
+            <div class="w-52 my-auto flex place-content-center mb-4 sm:mb-auto">
                 <ThemeToggle/>
             </div>
         </footer>
