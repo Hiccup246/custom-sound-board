3c04e65aeb05656e3139f8844e514ce8ba577dd2
James Watt <james.watt529@outlook.com>
2024-03-24 02:01:54 +0000

    Refactor theme toggle to not have so many nested divs

--- islands/ThemeToggle.tsx
+++ islands/ThemeToggle.tsx
@@ -29,21 +29,19 @@ export default function ThemeToggle() {
 
   return (
     <div onClick={toggleTheme}>
-      <label class="flex items-center cursor-pointer">
-        <div class="relative">
-          <input
-            type="checkbox"
-            class="sr-only"
-            onClick={(event) => event.stopImmediatePropagation()}
-          />
-          <div class="block dark:bg-white bg-white w-14 h-5 rounded-full"></div>
-          <div class="dot absolute left-1 top-1 bg-black w-6 h-3 rounded-full transition">
-          </div>
+      <label class="flex items-center cursor-pointer relative">
+        <input
+          type="checkbox"
+          class="sr-only"
+          onClick={(event) => event.stopImmediatePropagation()}
+        />
+        <div class="block dark:bg-white bg-white w-14 h-5 rounded-full"></div>
+        <div class="dot absolute left-1 bg-black w-6 h-3 rounded-full transition">
         </div>
 
-        <p class="ml-3 dark:text-white text-black">
+        <div class="ml-3 dark:text-white text-black">
           {theme}
-        </p>
+        </div>
       </label>
     </div>
   );
